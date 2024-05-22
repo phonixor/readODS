7289c7d0e31efe00f39e75b68eb09f69dc654585
chainsawriot <chainsawtiney@gmail.com>
2024-05-22 15:38:44 +0200

    use `minty` ref #173 (#187)
    * experiment with minty
    
    * minty is on CRAN

--- R/read_ods.R
+++ R/read_ods.R
@@ -139,22 +139,11 @@
     return(data.frame())
 }
 
-.type_convert <- function(df, col_types = NULL, verbose = TRUE, na = c("", "NA"), trim_ws = TRUE) {
-    if (verbose) {
-        res <- readr::type_convert(df = df, col_types, na = na)
-    } else {
-        suppressMessages({
-            res <- readr::type_convert(df = df, col_types, na = na, trim_ws = trim_ws)
-        })
-    }
-    return(res)
-}
-
 .handle_col_types <- function(res, col_types, verbose, na, trim_ws) {
     if (isTRUE(is.na(col_types)) || nrow(res) == 0) {
         return(res)
     }
-    .type_convert(df = res, col_types = col_types, verbose = verbose, na = na, trim_ws = trim_ws)
+    minty::type_convert(df = res, col_types = col_types, verbose = verbose, na = na, trim_ws = trim_ws)
 }
 
 ## standardise `sheet` parameter as a number, i.e. sheet_index
