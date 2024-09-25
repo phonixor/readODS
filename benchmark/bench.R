51770c83400ea1fa0d995e293a09959afde6bd12
chainsawriot <chainsawtiney@gmail.com>
2024-09-25 13:42:12 +0200

    roundtrip

--- benchmark/bench.R
+++ benchmark/bench.R
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-saveRDS(bench::mark(readODS::write_ods(nycflights13::flights), min_iterations = 2), paste0("benchmark/", args[1], "_write_ods_.RDS"))
+saveRDS(bench::mark(readODS::read_ods(readODS::write_ods(nycflights13::flights)), min_iterations = 3), paste0("benchmark/", args[1], "_write_ods_.RDS"))
--- benchmark/print_bench.R
+++ benchmark/print_bench.R
@@ -4,6 +4,6 @@ prefixes <- c("HEAD", "PR")
 ## TODO: make this a GH comment
 
 for (prefix in prefixes) {
-    print(prefix)
-    print(readRDS(paste0("benchmark/", prefix, "_write_ods_.RDS"))$median)
+    cat(prefix, "\n")
+    cat(readRDS(paste0("benchmark/", prefix, "_write_ods_.RDS"))$median, "\n")
 }
