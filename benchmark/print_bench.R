17821233a5a95db3f5a2cf1c989e3b1a63a60157
chainsawriot <chainsawtiney@gmail.com>
2024-09-25 12:25:29 +0200

    Test 4

--- benchmark/print_bench.R
+++ benchmark/print_bench.R
@@ -1,8 +1,9 @@
+library(bench)
 prefixes <- c("HEAD", "PR")
 
 ## TODO: make this a GH comment
 
 for (prefix in prefixes) {
-    print(head)
-    print(readRDS(paste0("benchmark/", prefix, "_write_ods_.RDS")))
+    print(prefix)
+    print(readRDS(paste0("benchmark/", prefix, "_write_ods_.RDS"))$median)
 }
